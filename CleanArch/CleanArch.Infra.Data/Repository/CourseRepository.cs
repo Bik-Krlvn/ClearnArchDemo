12db64725ce76b057de0bc84bcc2883a1aa1621b
bik_krlvn <kelvinbirikorang@gmail.com>
2019-12-10 02:33:43 +0000

    commit impl of service and repository

---
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CleanArch.Domain.Entities;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Data.Context;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CourseRepository:ICourseRepository
+    {
+        private CleanDBContext _cleanDbContext;
+
+        public CourseRepository(CleanDBContext cleanDbContext)
+        {
+            _cleanDbContext = cleanDbContext;
+
+        }
+        public IEnumerable<Course> GetCourses()
+        {
+            return _cleanDbContext.Courses;
+        }
+    }
+}
